611483807045c0d8b41831af8342a688d2db21d4
LTrashy <unladronv4@gmail.com>
2022-03-30 22:56:42 -0500

    connect api

--- django-mysql-api/Proyecto_API/Proyecto_API/settings.py
+++ django-mysql-api/Proyecto_API/Proyecto_API/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api',
 ]
 
@@ -48,7 +49,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'Proyecto_API.urls'
 
