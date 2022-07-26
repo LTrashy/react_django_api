ccf1f381b855848930f3d4af8cda3d19c4691cc8
Ander Gonzalez <andersongonzalez2001@gmail.com>
2022-07-26 00:38:49 -0500

    create docker files and docker compose

--- django-mysql-api/Proyecto_API/api/models.py
+++ django-mysql-api/Proyecto_API/api/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class Company(models.Model):
-    name=models.CharField(max_length=50)
-    direccion=models.CharField(max_length=100, default="")
-    nit=models.PositiveBigIntegerField(default=0)
-    telefono=models.PositiveBigIntegerField(default=0)
+    name = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100, default="")
+    nit = models.PositiveBigIntegerField(default=0)
+    telefono = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- react-crud-companies/src/components/Company/companyServer.js
+++ react-crud-companies/src/components/Company/companyServer.js
@@ -1,45 +1,45 @@
-// const API_URL = "http://localhost:8000/api/companies/";
-const API_URL = "https://companies-test.herokuapp.com/api/companies/";
+const API_URL = "http://localhost/api/companies/";
+// const API_URL = "https://companies-test.herokuapp.com/api/companies/";
 
-export const listCompanies= async ()=>{
-    return await fetch(API_URL);
+export const listCompanies = async () => {
+  return await fetch(API_URL);
 };
-export const getCompany= async (companyId)=>{
-    return await fetch(`${API_URL}${companyId}`);
-};
-
-export const registerCompany= async (newCompany)=>{
-    return await fetch(API_URL,{
-        method:"POST",
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({
-            "name":String(newCompany.name).trim(),
-            "direccion":String(newCompany.direccion).trim(),
-            "nit":parseInt(newCompany.nit),
-            "telefono":parseInt(newCompany.telefono),
-        }),
-    });
+export const getCompany = async (companyId) => {
+  return await fetch(`${API_URL}${companyId}`);
 };
 
-export const updateCompany= async (companyId, updatedCompany)=>{
-    return await fetch(`${API_URL}${companyId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: String(updatedCompany.name).trim(),
-        direccion: String(updatedCompany.direccion).trim(),
-        nit: parseInt(updatedCompany.nit),
-        telefono: parseInt(updatedCompany.telefono),
-      }),
-    });
+export const registerCompany = async (newCompany) => {
+  return await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: String(newCompany.name).trim(),
+      direccion: String(newCompany.direccion).trim(),
+      nit: parseInt(newCompany.nit),
+      telefono: parseInt(newCompany.telefono),
+    }),
+  });
 };
 
-export const DeleteCompany= async (companyId)=>{
-    return await fetch(`${API_URL}${companyId}`,{
-        method:"DELETE",
-        });
+export const updateCompany = async (companyId, updatedCompany) => {
+  return await fetch(`${API_URL}${companyId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: String(updatedCompany.name).trim(),
+      direccion: String(updatedCompany.direccion).trim(),
+      nit: parseInt(updatedCompany.nit),
+      telefono: parseInt(updatedCompany.telefono),
+    }),
+  });
+};
+
+export const DeleteCompany = async (companyId) => {
+  return await fetch(`${API_URL}${companyId}`, {
+    method: "DELETE",
+  });
 };
